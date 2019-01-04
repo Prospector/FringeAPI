6d985d6531316b4e474c464aab11b6b271a4ca93
ProfessorProspector <thedoctorsoda@gmail.com>
2019-01-04 14:18:00 -0800

    Update

---
+++ src/main/java/io/github/prospector/fringe/Fringe.java
@@ -0,0 +1,15 @@
+package io.github.prospector.fringe;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Fringe implements ModInitializer {
+
+	static {
+
+	}
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
